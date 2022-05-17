8faa20baf1098bfb042226398c4458b5696ec274
mhtaldmr <mht.alidemir@gmail.com>
2022-05-17 08:33:28 +0300

    Circuit entity has created.

--- MuhammetAliDemir.TP.Week1.Homework/Entity/Circuit.cs
+++ MuhammetAliDemir.TP.Week1.Homework/Entity/Circuit.cs
@@ -1,6 +1,18 @@
-﻿namespace MuhammetAliDemir.TP.Week1.Homework.Entity
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MuhammetAliDemir.TP.Week1.Homework.Entity
 {
     public class Circuit
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double Length { get; set; }
+        public int NumberOfLaps { get; set; }
+        public double LapRecord { get; set; }
+        public DateTime FirstRace { get; set; }
     }
 }
