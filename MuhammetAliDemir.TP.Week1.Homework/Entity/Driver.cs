27fe9ecdfa79790a229f58bf533e56de6fcf862b
mhtaldmr <mht.alidemir@gmail.com>
2022-05-17 08:30:09 +0300

    Driver entity has created.

--- MuhammetAliDemir.TP.Week1.Homework/Entity/Driver.cs
+++ MuhammetAliDemir.TP.Week1.Homework/Entity/Driver.cs
@@ -1,6 +1,17 @@
-﻿namespace MuhammetAliDemir.TP.Week1.Homework.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MuhammetAliDemir.TP.Week1.Homework.Entity
 {
     public class Driver
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public string Nationality { get; set; }
+        public int RaceEntered { get; set; }
+        public int Podiums { get; set; }
+        public int Championship { get; set; }
     }
 }
