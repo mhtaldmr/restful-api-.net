8209f2b79347ee55dd9d7fb5a691a1b000cd3312
mhtaldmr <mht.alidemir@gmail.com>
2022-05-17 08:32:19 +0300

    Team entity has created.

--- MuhammetAliDemir.TP.Week1.Homework/Entity/Team.cs
+++ MuhammetAliDemir.TP.Week1.Homework/Entity/Team.cs
@@ -1,6 +1,18 @@
-﻿namespace MuhammetAliDemir.TP.Week1.Homework.Entity
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MuhammetAliDemir.TP.Week1.Homework.Entity
 {
     public class Team
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string PowerUnit { get; set; }
+        public string Country { get; set; }
+        public string TeamChief { get; set; }
+        public List<Driver> Drivers { get; set; }
+        public int Championship { get; set; }
     }
 }
